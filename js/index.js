21c277a2a7d6a1591102a0e861d4dc55624cae50
davidgortizjr <nyxdesu3@gmail.com>
2025-03-05 23:03:14 +0800

    overhaul

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const overlay = document.createElement("div");
+overlay.classList.add("background-overlay");
+document.body.appendChild(overlay);
+
+document.querySelector(".school").addEventListener("mouseover", function () {
+    overlay.style.backgroundImage = "url('imgs/schoolBG.jpg')";
+    overlay.style.opacity = "1";
+});
+
+document.querySelector(".school").addEventListener("mouseout", function () {
+    overlay.style.opacity = "0";
+});
+
+document.querySelector(".personal").addEventListener("mouseover", function () {
+    overlay.style.backgroundImage = "url('imgs/personalBG.jpeg')";
+    overlay.style.opacity = "1";
+});
+
+document.querySelector(".personal").addEventListener("mouseout", function () {
+    overlay.style.opacity = "0";
+});
