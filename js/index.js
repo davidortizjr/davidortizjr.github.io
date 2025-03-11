fdfda5a102f5940527ed70610a45017457e160c8
davidgortizjr <nyxdesu3@gmail.com>
2025-03-12 02:25:12 +0800

    overhauled my index for school

--- js/index.js
+++ js/index.js
@@ -19,3 +19,12 @@ document.querySelector(".personal").addEventListener("mouseover", function () {
 document.querySelector(".personal").addEventListener("mouseout", function () {
     overlay.style.opacity = "0";
 });
+
+document.querySelector(".about").addEventListener("mouseover", function () {
+    overlay.style.backgroundImage = "url('imgs/personalBG.jpeg')";
+    overlay.style.opacity = "1";
+});
+
+document.querySelector(".about").addEventListener("mouseout", function () {
+    overlay.style.opacity = "0";
+});
