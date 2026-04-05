5bcb9dde9394b5fa4aa8805de538838d005d1589
max <nyxdesu3@gmail.com>
2026-04-05 17:33:26 +0800

    fixed css

--- src/components/DesktopShortcuts.tsx
+++ src/components/DesktopShortcuts.tsx
@@ -39,10 +39,8 @@ const DesktopShortcuts = ({ onOpenFile }: DesktopShortcutsProps) => {
             onClick={() => handleShortcutClick(shortcut.id)}
             disabled={!canOpen}
           >
-            <span className={`desktop-icon-tile ${shortcut.tone}`}>
-              <span className="desktop-icon-glyph-wrap">
-                <Icon size={30} strokeWidth={2.1} className="desktop-icon-glyph" />
-              </span>
+            <span className={`desktop-icon-glyph-wrap ${shortcut.tone}`}>
+              <Icon size={30} strokeWidth={2.1} className="desktop-icon-glyph" />
             </span>
             <span className="desktop-icon-label">{shortcut.label}</span>
           </button>
